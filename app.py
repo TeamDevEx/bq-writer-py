01fe10a8400fffd172290ff1dfc0a913a17161ca
Lendly Cagata <lendly.cagata@telus.com>
2023-12-14 22:54:30 +0800

    chore: 2nd

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import json
+import uuid
+import config
+
 from datetime import datetime
 
-from flask import Flask, request
+from flask import Flask, jsonify, request , g
 from google.cloud import bigquery
 
 
@@ -10,6 +13,22 @@ app = Flask(__name__)
 
 bigquery_client = bigquery.Client()
 
+@app.before_request
+def before_request_func():
+    execution_id = uuid.uuid4()
+    g.start_time = datetime.utcnow().isoformat()  
+    g.execution_id = execution_id
+    print(g.execution_id, "ROUTE CALLED ", request.url)
+
+@app.after_request
+def after_request(response):
+    if response and response.get_json():
+        data = response.get_json()
+        data["time_request"] = datetime.utcnow().isoformat()  
+        data["version"] = config.VERSION
+        response.set_data(json.dumps(data))
+
+    return response
 @app.route("/cloudEvent", methods=["POST"])
 def cloud_event():
     timestamp = datetime.utcnow().isoformat()    
@@ -28,10 +47,11 @@ def cloud_event():
         bigquery_client.insert_rows(table, rows)
 
         print("PAYLOAD WAS INGESTED SUCCESSFULLY")
-        return "Payload ingested successfully", 200
+        return jsonify({"status": 200 , "message": "Payload ingested successfully"})
     except Exception as e:
         print("SOMETHING WENT WRONG:", e)        
-        return "Internal Server Error", 500
+        return jsonify({"status": 500 , "message": "Internal Server Error"})
     
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", config.PORT)))
+    
